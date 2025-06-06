fb956159385088f9831efd7ab2c6e897fee4d24c
ahmedehab72 <a01011618272@gmail.com>
2025-06-06 05:13:41 +0300

    add use client to navbar and auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,19 @@
-import {  SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+"use client"
+import { SignedIn, SignedOut, SignInButton, UserButton, useAuth } from '@clerk/nextjs'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import { ModeToggle } from './ModeToggle'
 
 export default function Navbar() {
+  const { isSignedIn, isLoaded } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.replace('/sign-in');
+    }
+  }, [isLoaded, isSignedIn, router]);
+
   return (
     <header className='flex items-center justify-between p-24 py-6 '>
       <ModeToggle />
