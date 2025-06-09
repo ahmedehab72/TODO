93092aee6a3df8f991f8e4843af74808e9f8877c
ahmedehab72 <a01011618272@gmail.com>
2025-06-09 08:11:27 +0300

    delete logs

--- components/ClientUserId.tsx
+++ components/ClientUserId.tsx
@@ -9,17 +9,13 @@ export default function ClientUserId() {
     useEffect(() => {
         // Only run when Clerk has finished loading
         if (!isLoaded) return
-        
-        console.log('ClientUserId - isLoaded:', isLoaded, 'isSignedIn:', isSignedIn, 'userId:', userId)
-        
+                
         if (isSignedIn && userId) {
             // User is signed in - set the cookie
             document.cookie = `userId=${userId}; path=/; max-age=${60 * 60 * 24 * 7}; SameSite=Lax`
-            console.log('Cookie set for userId:', userId)
         } else {
             // User is signed out - delete the cookie
             document.cookie = 'userId=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-            console.log('Cookie deleted - user signed out')
         }
     }, [isLoaded, isSignedIn, userId])
 
